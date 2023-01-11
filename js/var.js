1baa43016384c5bf5dc5968450036ba17951dcd2
DivyamSingh <64833579+DivyamSingh18@users.noreply.github.com>
2023-01-12 00:19:06 +0530

    Update var.js

--- js/var.js
+++ js/var.js
@@ -1,1 +1,1 @@
-var link = "https://drive.google.com/file/d/1eKADxKFHeNgP4z74UTDdkSA1q8GCCyzj/view?usp=sharing"
+const link = "https://drive.google.com/file/d/1eKADxKFHeNgP4z74UTDdkSA1q8GCCyzj/view?usp=sharing"
